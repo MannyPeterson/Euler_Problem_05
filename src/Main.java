855a9716662a4836167068098088c64368da0df8
Manny Peterson <me@mannypeterson.com>
2019-04-19 18:29:52 -0500

    update

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Manny Peterson's Solution to Project Euler Problem #5
+ * https://projecteuler.net/problem=5
+ * 
+ * Enjoy! :)
+ * 
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
