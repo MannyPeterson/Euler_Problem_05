155f8969c8d419e201d0f6906fe0482cb571aadd
Manny Peterson <me@mannypeterson.com>
2019-04-19 18:40:21 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,18 @@
  */
 
 public class Main {
-	public static void main(String[] args) {
+	private static boolean isDivisible(int n, int start, int finish) {
+		for (int i = start; i <= finish; i++)
+			if (n % i != 0)
+				return false;
+		return true;
+	}
 
+	public static void main(String[] args) {
+		int i;
+		for (i = 21; i > 0; i++)
+			if (Main.isDivisible(i, 1, 20))
+				break;
+		System.out.format("ANSWER = %d\n", i);
 	}
 }
